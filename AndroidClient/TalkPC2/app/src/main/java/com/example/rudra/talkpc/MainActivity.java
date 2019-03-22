14583cdf19bd91c249ba0137d9fe877f6373d891
phantom5 <rickrudra@gmail.com>
2019-03-22 08:15:18 +0530

    don't really care about commit counts by now

---
+++ AndroidClient/TalkPC2/app/src/main/java/com/example/rudra/talkpc/MainActivity.java
@@ -0,0 +1,118 @@
+package com.example.rudra.talkpc;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.DataOutput;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.sql.Connection;
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+    ImageButton send;
+    TextView tv;
+    EditText et;
+    static int control=0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        send=findViewById(R.id.button);
+        tv=findViewById(R.id.textView3);
+        et=findViewById(R.id.editText2);
+        tv.setText("");
+
+        et.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String  s=et.getText().toString();
+                if(s.equals("Talk to PC")){
+                    et.setText("");
+                }
+            }
+        });
+
+
+        send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String s=et.getText().toString();
+                if(s.equals("")||s.equals("Talk to PC"))
+                {
+                    tv.setText("I Didn't Undertsand You");
+                }else{
+                   final String cmd=et.getText().toString();
+                   Thread t=new Thread(new Runnable() {
+                       @Override
+                       public void run() {
+                           try {
+                               URL url = new URL("https://api.myjson.com/bins/11tt7a");
+                               HttpURLConnection connection=(HttpURLConnection)url.openConnection();
+                               connection.setRequestMethod("PUT");
+                               connection.setRequestProperty("Content-Type","application/json");
+                               connection.setDoOutput(true);
+                               connection.setDoInput(true);
+
+                               JSONObject jsOb=new JSONObject();
+                               jsOb.put("sup_a",cmd);
+                               DataOutputStream dos=new DataOutputStream(connection.getOutputStream());
+                               dos.writeBytes(jsOb.toString());
+                               Log.d("ConTest",Integer.toString(connection.getResponseCode()));
+                               dos.flush();
+                               dos.close();
+                               connection.disconnect();
+                               while(true) {
+                                   connection = (HttpURLConnection) url.openConnection();
+                                   connection.setRequestMethod("GET");
+                                   BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                                   String inp;
+                                   StringBuffer resp = new StringBuffer();
+                                   while ((inp = in.readLine()) != null) {
+                                       resp.append(inp);
+                                   }
+                                   if(resp.indexOf("sup_p")>=0){
+                                       Log.d("ConTest",resp.toString());
+                                       final String resp_inner=resp.toString();
+                                       runOnUiThread(new Runnable() {
+                                           @Override
+                                           public void run() {
+                                               tv.setText(resp_inner.substring(10,resp_inner.length()-2));
+                                           }
+                                       });
+                                   break;
+                                   }
+                                   in.close();
+                                   connection.disconnect();
+                               }
+
+                           }catch(Exception e){
+                               Log.d("ConTest","Caught in send.onClick");
+                           }
+                       }
+                   });
+                   t.start();
+
+                }
+
+            }
+        });
+
+
+    }
+}
---
+++ run.py
@@ -0,0 +1,93 @@
+import requests
+import os
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
+import time
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+
+
+'''#####################################################################################################################
+# Date of Submission -                                                                                                 #
+# By - Rudra Madhav Biswal                                                                                             #
+# 1601106117 , IT - 6th SEM                                                                                            #
+# CNDC Assignment                                                                                                      # 
+#####################################################################################################################'''
+
+
+url = 'https://api.myjson.com/bins/11tt7a'
+headers = {'content-type': 'application/json'}
+temp="nothing"
+
+while True:
+
+    req = requests.get(url)
+    data_received = req.json()
+
+    if "sup_a" in data_received:
+        data_received_string = data_received["sup_a"]
+        print(data_received_string)
+        if len(data_received_string) != 0:
+            print ("Command Received")
+
+        if "shutdown" in data_received_string:
+            payload = {"sup_p": "Shutting Down PC"}
+            response = requests.put(url, json=payload)
+            print(response.status_code)
+            os.system('shutdown /p /f')
+
+        elif "send me file" in data_received_string:
+            payload = {"sup_p": "I am on it"}
+            response = requests.put(url, json=payload)
+            print(response.status_code)
+            print(data_received_string)
+            filename=data_received_string[13:]
+            print(filename)
+            print("Attaching File...")
+            sender_email = "talkPC2019@gmail.com"
+            receiver_email = "rickrudra@gmail.com"
+            msg = MIMEMultipart()
+            msg['From'] = sender_email
+            msg['To'] = receiver_email
+            msg['Subject'] = "Here's the file you requested"
+            body = "See Attachment"
+            msg.attach(MIMEText(body, 'plain'))
+            attachment = open(filename, "rb")
+            p = MIMEBase('application', 'octet-stream')
+            p.set_payload(attachment.read())
+            encoders.encode_base64(p)
+            p.add_header('Content-Disposition', 'attachment;filename='+filename)
+            msg.attach(p)
+            s = smtplib.SMTP('smtp.gmail.com', 587)
+            s.starttls()
+            s.login(sender_email, "emophheg@24")
+            text = msg.as_string()
+            print("Sending Mail...")
+            s.sendmail(sender_email, receiver_email, text)
+            s.quit()
+            print("Mail Has Been Sent")
+
+        elif "play" in data_received_string:
+            payload = {"sup_p":"Now Playing"}
+            response=requests.put(url,json=payload)
+            print(response.status_code)
+            find_item = data_received_string[5:]
+            driver = webdriver.Firefox()
+            driver.get(find_item)
+            wait = WebDriverWait(driver,5);
+            time.sleep(10)
+            find = wait.until(EC.visibility_of_element_located((By.CLASS_NAME, "html5-main-video")))
+            find.click()
+            time.sleep(2)
+            find = wait.until(EC.visibility_of_element_located((By.CLASS_NAME,"ytp-fullscreen-button")))
+            find.click()
+            time.sleep(2)
+            find = wait.until(EC.visibility_of_element_located((By.CLASS_NAME,"html5-main-video")))
+            find.click()
+
+    time.sleep(1)
